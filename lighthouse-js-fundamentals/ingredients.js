94d5f88e452713abda7d52300d5b5daaff050324
Baila3 <98368775+Baila3@users.noreply.github.com>
2022-03-18 16:01:46 -0400

    ingredients

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,16 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+while (i < ingredients.length){
+console.log(ingredients[i]);
+i++;
+}
+
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+for (let i = 0; i < ingredients.length; i++) {
+ingredients.reverse()
+  console.log(ingredients[i]);
+}
+
+
